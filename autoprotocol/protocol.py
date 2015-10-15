028186746ebd4cb4b5eb6050fabc17fe73836b84
Tali Herzka <tali@herzka.com>
2015-10-15 16:36:31 -0700

    make educated guess about what cover type should be used based on previous instructions

--- autoprotocol/protocol.py
+++ autoprotocol/protocol.py
@@ -1836,6 +1836,7 @@ class Protocol(object):
                     # Initialize new stamp list/instruction
                     self.instructions.append(Stamp([trans]))
 
+
     def sangerseq(self, cont, wells, dataref, type="standard", primer=None):
         """
         Send the indicated wells of the container specified for Sanger sequencing.
